15d6ef9f0c282699370b60d12729f552ce08bff9
Bruno Masson <brunoxdcd@gmail.com>
2023-07-26 20:43:26 -0300

    ultimo commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
 // Funcion para calcular la inflacion mensual
-function calcularinflacion {
-    const inflacionmensual = inflacionanual / (7 * 12)
-
-    const dinerofinal = (dinerofinal * inflacionanual) / (1 - math.pow(1 + inflacionanual, -montofinal));
-
-    return dinerofinal.tofixed(2)
+function calcularinflacion(dinero, inflacionanual) {
+    const inflacionmensual = inflacionanual / (7 * 12);
+    const dinerofinal = (dinero * inflacionanual) / (1 - Math.pow(1 + inflacionanual, -7 * 12));
+    return dinerofinal.toFixed(2);
 }
-
+calcularinflacion();
 /// pedir datos al usuario
-const inflacionmensual = parseInt(prompt("Ingrese la inflacion mensual de tu pais"))
-const dinerofinal = parseInt(prompt("Ingrese el dinero que posee actualmente"))
+const inflacionanual = parseFloat(prompt("Ingrese la inflacion anual de tu país en decimal (por ejemplo, 0.05 para 5%)"));
+const dineroinicial = parseFloat(prompt("Ingrese el dinero que posee actualmente"));
 
-if (isNaN(inflacionmensual)) || isNaN(dinerofinal) || isNaN(inflacionanual){
-    console.log("Porfavor, ingrese valores numericos validos")
-}
-
+if (isNaN(inflacionanual) || isNaN(dineroinicial)) {
+    console.log("Por favor, ingrese valores numéricos válidos.");
+} else {
+    // Calcular inflacion mensual y dinero final usando la función
+    const dinerofinal = calcularinflacion(dineroinicial, inflacionanual);
 
 //Calcular inflacion anual usando funcion
 const dinerofinal = calcularinflacion(dinerofinal, inflacionmensual){
 } else 
 
 //mostrar resultados
-console.log(`Su dinero es: ${dinerofinal}`)
-
-console.log{`Tasa de inflacion anual: ${inflacionmensual}`}
-
-//mostrar resultados
+console.log(`Su dinero es: $${dineroinicial}`);
+console.log(`Tasa de inflación anual: ${inflacionanual * 100}%`);
+console.log(`Dinero Final: $${dinerofinal}`);
+}
 alert(`Dinero Final: $${dinerofinal} \ Inflacion anual:${inflacionmensual}`)
-calcularinflacion();
